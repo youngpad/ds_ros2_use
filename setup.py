f1638ccccdd75b6559859a0af2cc6dfcc6706135
Martin Maeland <31969005+martinmaeland@users.noreply.github.com>
2021-03-27 21:54:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'ds_ros2_use'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,9 +13,9 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='martin',
+    maintainer='martinmaealand',
     maintainer_email='martinmaeland@outlook.com',
-    description='TODO: Package description',
+    description='ROS2 node for transfering data between ground station and drone.',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
